51b43f0abbd55f1b65919a7d3ed55071de9d6ae4
rohrasumeet <sumetrohra1@gmail.com>
2022-09-06 17:52:13 +0530

    feat: initialize MUI

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const App = () => {
-  return <div>Hello ethonline 2022</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div>Hello ethonline 2022</div>
+    </ThemeProvider>
+  );
 };
 
 export default App;
